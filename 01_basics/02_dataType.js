cdeebf8a1225895e8b09668a51b303eab4a52ab6
saurbh-singh <159182182+saurbh-singh@users.noreply.github.com>
2024-07-25 12:09:59 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+
+//alert(3 + 3) we are useing nodejs, not browser
+
+console.log(3+3)
+console.log("saurabh")
+
+
+let name = "saurabh"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigInt(ka use trading)
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol =>unique
+
+
+//object
+
+console.log(typeof  "saurabh");   // string
+console.log(typeof  age);   // number
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   // object
