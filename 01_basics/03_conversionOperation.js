e404fdf55ca3bbf333983b5dd612658a8fb42395
saurbh-singh <159182182+saurbh-singh@users.noreply.github.com>
2024-07-26 10:55:25 +0000

    Datatypeconversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,25 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(stringNumber);
+//*************Operations**************//
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2  ="hi"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 +"2");
+console.log("1" +2 +2);
+console.log(1+2+"2");
