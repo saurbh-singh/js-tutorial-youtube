2b9b5cbf871b8d11da1e96d22b37817dffdf288b
saurbh-singh <159182182+saurbh-singh@users.noreply.github.com>
2024-07-26 10:24:51 +0000

    Datatypeconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33abc"
+console.log(score);
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+// "33" =>33
+// "33abc"  => NaN
+//true => 1 ; false =>0
+
+let isLoggedIn = "saurabh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true ; 0=>false
+// ""  => false
+//"saurabh"  => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(stringNumber);
