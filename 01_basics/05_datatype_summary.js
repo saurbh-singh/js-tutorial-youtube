9f244b9de9c169f0fdfe2cb2a94dbb3d7a84d369
saurbh-singh <159182182+saurbh-singh@users.noreply.github.com>
2024-07-27 05:56:34 +0000

    Datatypesummary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,45 @@
+//  Primitive
+
+
+
+// 7 types : String , Number, Boolean, null , undefined , Symbol,
+//  BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+console.log(id === anotherId);  //false
+
+const bigNumber = 345556677777n;
+// Reference (Non primitive)
+
+// Array, Objects, Functions 
+
+const heros = ["shaktiman","nagraj" ,"doga"]
+
+let myobj ={
+   name:"saurabh",
+   age:22,
+}
+
+ const myfuction=function (){
+
+    console.log("Hello World");
+ }
+
+
+ console.log(typeof bigNumber);  //bigint
+
+
+
+
+ //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Masterey in js (Object and browser web event)
