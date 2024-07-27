ba61d8efb494d757aa83c62c29fb6013508191bd
saurbh-singh <159182182+saurbh-singh@users.noreply.github.com>
2024-07-27 04:48:11 +0000

    Datatypeconversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,7 +35,27 @@ let str2  ="hi"
 let str3 = str1 + str2
 console.log(str3);
 
-console.log("1" + 2);
-console.log(1 +"2");
-console.log("1" +2 +2);
-console.log(1+2+"2");
+console.log("1" + 2);    //12
+console.log(1 +"2");      //12
+console.log("1" +2 +2);  //122
+console.log(1+2+"2");  //32
+
+
+console.log(  (3 + 4) * 4 % 3);
+
+console.log(true);
+console.log(+true)
+console.log(+"");
+console.log("");
+  
+
+let num1 , num2, num3
+num1= num2 = num3 =2+2
+
+
+let gameCounter = 100
+  gameCounter++;
+  console.log(gameCounter);
+
+  // prifix and postfix  js mdn site more knowledge
+
