e67bd7c4457dce78930ca6fcb92cc09800340b42
saurbh-singh <159182182+saurbh-singh@users.noreply.github.com>
2024-07-27 05:11:06 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);    //js change  datatype 
+console.log("02" > 1);
+
+//  this type code is not clean code
+  console.log(null >0);   //false
+  console.log(null == 0);   //false
+  console.log(null >=0);     //true
+
+
+  console.log(undefined  == 0);
+  console.log(undefined  > 0);
+  console.log(undefined < 0);
+
+
+  // === (strict check)
+    console.log("2" === 2);
